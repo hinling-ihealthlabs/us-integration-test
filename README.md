20c8f7688a9524ac0b6fd1bd059b7e182535c8ba
hinling yeung <hinling.yeung@ihealthlabs.com>
2017-06-14 13:54:38 -0700

    add dev.ie config and README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+To run the tests locally:
+
+./node_modules/.bin/wdio  ./config/wdio.conf.local.js
+
+To run the tests on our selenium hub (with IE browser):
+
+./node_modules/.bin/wdio ./config/wdio.conf.dev.ie.js
---
+++ config/wdio.conf.dev.ie.js
@@ -0,0 +1,11 @@
+'use strict'
+const base = require('./wdio.conf.base.js');
+exports.config = Object.assign(base.config, {
+  host: '52.41.120.147',
+  port: 32768,
+  capabilities: [{
+    //browserName: 'firefox'
+    browserName: 'internet explorer'
+  }],
+  baseUrl: 'http://web.bettercare.dev.ihealthnext.com',
+});
